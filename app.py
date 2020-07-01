d8d2df28d5fb1b9211a627f4b4f80c5df0592e7d
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2020-07-01 18:52:32 +0200

    added login and register form

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 
-trainings = [
+app.config["SECRET_KEY"] = "c4d269c24985531076eb5020c1c221a5ex"
+
+records = [
     {
         "user": "Eric Rockstädt",
         "type": "Swimming",
@@ -25,12 +28,29 @@ trainings = [
 @app.route('/')
 @app.route("/home")
 def home():
-    return render_template("home.html", trainings=trainings)
+    return render_template("home.html", records=records)
 
 
 @app.route("/about")
 def about():
     return "Hello about"
+
+
+@app.route("/trainings")
+def trainings():
+    return render_template("trainings.html")
+
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
+
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 
 if __name__ == '__main__':
